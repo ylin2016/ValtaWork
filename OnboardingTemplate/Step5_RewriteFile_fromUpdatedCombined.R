99354e95a8840e1fe99bf3ed6912731084a4ccf2
ylin2016 <nklinyi@gmail.com>
2026-04-12 11:29:50 -0700

    Update and add new step to write out

--- OnboardingTemplate/Step5_RewriteFile_fromUpdatedCombined.R
+++ OnboardingTemplate/Step5_RewriteFile_fromUpdatedCombined.R
@@ -8,18 +8,37 @@ library(lubridate)
 setwd("/Users/ylin/Google Drive/My Drive/Data and Reporting/10-OnboardingTemplate/")
 property.file = '/Users/ylin/Google Drive/My Drive/Data and Reporting/Data/Property_Cohost.xlsx'
 property = read.xlsx(property.file)
-allfiles=data.frame(filename=list.files(path="./PropertyInformation/")[-1])
-allfiles$Listing = sub(".xlsx","",allfiles$filename)
+
 filescombined = read.xlsx("masterfile_combined.xlsx")
-filescombined[grepl("Recycling Details",filescombined$Field,fixed=T),"Field"] = 
-  "Maintenance – Utilities – Garbage/Recycling Details/Location"
+filescombined$rnk =1:nrow(filescombined)
+
+urls = read.xlsx("HouseUrls.xlsx",sheet = "Link")
+urls$Field = "Account Link"
+urls$Field2 = "House manual link"
+urls$Field3 = "3D Link"
+filescombined = merge(filescombined,urls %>% select(nickname,Field,valtarealty),
+                      by.x=c("Property","Field"),by.y=c("nickname","Field"),all.x=T)
+
+filescombined = merge(filescombined,urls %>% select(nickname,Field2,houseManual),
+                      by.x=c("Property","Field"),by.y=c("nickname","Field2"),all.x=T)
+filescombined = merge(filescombined,urls %>% select(nickname,Field3,`3D.link`),
+                      by.x=c("Property","Field"),by.y=c("nickname","Field3"),all.x=T)
+filescombined = filescombined %>% 
+                mutate(target = ifelse(!is.na(target),target,
+                          ifelse(Field %in% "Account Link",valtarealty,
+                           ifelse(Field %in% "House manual link",houseManual,
+                            ifelse(Field %in% "3D Link",`3D.link`,target))))) %>%
+                arrange(rnk) 
+
 listings = unique(filescombined$Property)
+excluded = c("Bellevue 16237","Kirkland 10219","Seattle 1512")
+
 estimate_lines <- function(text, col_width_chars = 90) {
   text <- gsub("\r\n|\r", "\n", as.character(text))
   lines <- strsplit(text, "\n")[[1]]
   sum(sapply(lines, function(x) ceiling(nchar(x) / col_width_chars)))
 }
-for(k in listings[-2]) 
+for(k in setdiff(listings,excluded)) 
 {
   print(k)
   wb_out = loadWorkbook("PropertyFormat.xlsx")
@@ -34,15 +53,15 @@ for(k in listings[-2])
     arrange(order) %>%
     mutate(X2=Main.listing)
 
-  if(!is.na(data.sel[1,"Child.listing..if.any."])) 
+  if(!is.na(data.sel[1,"Child.listing.1"])) 
    {
-    grid_tpl0$X3 = grid_tpl0$Child.listing..if.any.
+    grid_tpl0$X3 = grid_tpl0$Child.listing.1
     grid_tpl[2,3]="Child.listing"
     }
   
-  if(!is.na(data.sel[1,"Child.listing..if.any..1"])) 
+  if(!is.na(data.sel[1,"Child.listing.2"])) 
     {
-      grid_tpl = grid_tpl0[,c("X1","X2","X3","Child.listing..if.any..1")]
+      grid_tpl = grid_tpl0[,c("X1","X2","X3","Child.listing.2")]
       grid_tpl[2,4]="Child.listing"
     }else{
       grid_tpl = grid_tpl0[,1:3]
@@ -61,28 +80,24 @@ for(k in listings[-2])
   
   writeData(wb_out, "Property", x = grid_tpl, colNames = FALSE)
   
-  links = grid_tpl0 %>% filter(grepl("http|.com",X2) | X1 %in% "Account Link") %>%
-    filter(grepl("Link|link",X1)) %>%
-    select(order,X1,X2,X3,url,url2) %>%
-    mutate(url = ifelse(is.na(url),X2,url),
-           name =sapply(X1,function(x) {
-             y=unlist(strsplit(x,"-"));y[length(y)]})) %>%
-    mutate(name=ifelse(name %in% "Account Link","Airbnb Link",name))
-  for(i in 1:nrow(links))
-  {
-    link2write = links$url[i]
-    names(link2write) = links$name[i]
-    class(link2write) <- "hyperlink"
-    writeData(wb_out, "Property",link2write,startRow = links$order[i]+1 ,startCol = 2)
-  }
-  if(!is.na(data.sel[1,"Child.listing..if.any."]))
-  {
+  links = grid_tpl0 %>% filter(!is.na(target)) %>% select(order,X1,X2,X3,target) 
+  if(nrow(links)>0){
     for(i in 1:nrow(links))
     {
-      link2write = links$X3[i]
-      names(link2write) = links$name[i]
+      link2write = links$target[i]
+      names(link2write) = links$X2[i]
       class(link2write) <- "hyperlink"
-      writeData(wb_out, "Property",link2write,startRow = links$order[i]+1 ,startCol = 3)
+      writeData(wb_out, "Property",link2write,startRow = links$order[i]+1 ,startCol = 2)
+    }
+    if(!is.na(data.sel[1,"Child.listing.1"]))
+    {
+      for(i in 1:nrow(links))
+      {
+        link2write = links$X3[i]
+        names(link2write) = links$name[i]
+        class(link2write) <- "hyperlink"
+        writeData(wb_out, "Property",link2write,startRow = links$order[i]+1 ,startCol = 3)
+      }
     }
   }
   listingfile = loadWorkbook(paste0("./PropertyInformation/",k,'.xlsx'))
@@ -90,7 +105,7 @@ for(k in listings[-2])
   owner = rbind.fill(owner[1,,drop=F],addlines,owner[-1,])
   writeData(wb_out, "Owner", x = owner, colNames = FALSE)
   
-  saveWorkbook(wb_out, paste0("./PropertyInformation/",k,'_upd.xlsx'), overwrite = TRUE)
+  saveWorkbook(wb_out, paste0("./Output/",k,' Onboarding Sheets.xlsx'), overwrite = TRUE)
 }  
 
 
