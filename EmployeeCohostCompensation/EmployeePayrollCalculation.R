1e9c6cbe776ee4202d740fdfe4f3ef4630d7c4fc
ylin2016 <nklinyi@gmail.com>
2026-09-19 21:18:40 -0700

    Updates

--- EmployeeCohostCompensation/EmployeePayrollCalculation.R
+++ EmployeeCohostCompensation/EmployeePayrollCalculation.R
@@ -45,10 +45,10 @@ ggplot(res,aes(data_collect,n,group = month,color=month)) +
   scale_x_datetime(date_break="15 days",labels = date_format("%m-%d")) +
   labs(x="date_collected",y="# of 5 star reviews",color="Created Month")
   
-k="20260816 guesty_reviews.xlsx"
+k="20260916 guesty_reviews.xlsx"
 
 ## Jackson's
-ratings[[k]] %>% filter(Overall %in% c(5,10) & grepl("2026-07",month)) %>% 
+ratings[[k]] %>% filter(Overall %in% c(5,10) & grepl("2026-08",month)) %>% 
   filter(!grepl("Cottage",nickname)) %>%
   group_by(month) %>% reframe(n=n())
 
